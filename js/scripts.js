20546c4b37f120ad65c08f8b306509232da1e615
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-02 10:22:00 -0700

    add subtract multiplication and division

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,33 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+      const number1 = parseInt($("#add1").val());
+      const number2 = parseInt($("#add2").val());
+      const result = add(number1, number2);
+      $("#output").text(result);
+    });
+ $("form#sub").submit(function(event) {
+   event.preventDefault();
+     const number1 = parseInt($("#sub1").val());
+     const number2 = parseInt($("#sub2").val());
+     const result = subtract(number1, number2);
+     $("#output").text(result);
+   });
+ $("form#mult").submit(function(event) {
+   event.preventDefault();
+     const number1 = parseInt($("#mult1").val());
+     const number2 = parseInt($("#mult2").val());
+     const result = multiply(number1, number2);
+     $("#output").text(result);
+   });  
+ $("form#div").submit(function(event) {
+  event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
